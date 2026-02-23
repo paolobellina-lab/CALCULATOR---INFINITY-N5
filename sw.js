04d10ba49d6beb6d5a84b43fb5fac5311313337b
paolobellina-lab <paolo.bellina@telmotor.it>
2026-02-23 21:42:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
-
+self.addEventListener('fetch', () => {});
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
